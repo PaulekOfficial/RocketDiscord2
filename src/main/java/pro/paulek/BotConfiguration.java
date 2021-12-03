b2ed47cdc26866c71d418e8a7e4ab7f7f22ac84d
Paweł Fortuna <paulekofficial@MacBook-Pro-Pawe.local>
2021-12-03 20:00:57 +0100

    Add dependencies

---
+++ src/main/java/pro/paulek/BotConfiguration.java
@@ -0,0 +1,10 @@
+package pro.paulek;
+
+import java.util.List;
+
+public class BotConfiguration {
+
+    private String endpoint;
+    List<String> adminIds;
+
+}
---
+++ src/main/java/pro/paulek/Main.java
@@ -0,0 +1,8 @@
+package pro.paulek;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
